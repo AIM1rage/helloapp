91343620d0e97c82ee19f032541b002b3a3d8ed3
white <white@example.com>
2022-11-02 22:00:22 +0500

    Всё по кайфу

--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -2,17 +2,31 @@
 
 namespace HelloApp
 {
-    internal class Program
+    public class Program
     {
         public static void GreetBlack()
         {
-            Console.WriteLine("hello black");   
+            Console.WriteLine("hello black");
+            var a = true;
+            if (a == true)
+            {
+                return;
+            }
+            else if (a == false)
+            {
+                return;
+            }
+            else
+            {
+                return;
+            }
         }
-        
+
         public static void GreetWhite()
         {
             Console.WriteLine("hello white");
         }
+        //обсудили)))
         
         public static void Main(string[] args)
         {
